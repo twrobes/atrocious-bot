3a4f9cf8cd1292724c450354e7117b6eb29e223a
Thomas Wrobel <twrobel115@gmail.com>
2024-07-13 00:09:55 -0500

    Finished /deathroll game - added check if someone challenges themselves - less message spam - roll button is only clickable by the player that has the current turn

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -14,45 +14,56 @@ class Games(commands.Cog):
         description='Start a deathroll with another user'
     )
     async def deathroll(self, interaction: discord.Interaction, opponent: discord.Member, roll: int):
-        await interaction.response.send_message(
-            f'{interaction.user.display_name} has challenged {opponent.display_name}!'
-        )
+        if interaction.user.id == opponent.id and interaction.user.id != 104797389373599744:
+            await interaction.response.send_message('You tried challenging yourself, how pathetic!')
+            return
+
+        starting_msg = \
+            f'{interaction.user.display_name} has challenged {opponent.display_name} to a deathroll starting at {roll}.'
+
+        await interaction.response.send_message(starting_msg)
         challenger = interaction.user
-        current_player = challenger
+
+        roll = random.randrange(1, roll + 1)
+        roll_history = f'{starting_msg}\n\n{challenger.display_name} rolled {roll}\n'
+        await interaction.edit_original_response(content=roll_history)
+        current_player = opponent
 
         while roll != 1:
-            view = Roll()
-
+            view = Roll(current_player)
             await interaction.followup.send(f'It is your turn to roll {current_player.display_name}.', view=view)
             await view.wait()
 
             roll = random.randrange(1, roll + 1)
-            await interaction.followup.send(f'{current_player.display_name} rolled {roll}')
+            roll_history += f'{current_player.display_name} rolled {roll}\n'
+            await interaction.edit_original_response(content=roll_history)
 
             if current_player is opponent:
                 current_player = challenger
             else:
                 current_player = opponent
 
-        print('winner')
-        await interaction.followup.send(f'**{current_player.display_name} wins!**')
+        roll_history += f'\n**{current_player.display_name} wins!**'
+        await interaction.edit_original_response(content=roll_history)
 
 
 class Roll(discord.ui.View):
-    def __init__(self):
+    def __init__(self, user: discord.Member):
         super().__init__()
         self.value = None
-        # self.user = user
+        self.user = user
 
-    # async def interaction_check(self, user: discord.Member):
-    #     # print(f'{user.id} {self.user.id}')
-    #     print(f'{user.name} {self.user.name}')
-    #     return user.name == self.user.name
+    async def interaction_check(self, interaction: discord.Interaction):
+        if interaction.user.id == self.user.id:
+            return True
+        else:
+            await interaction.response.send_message('It is not your turn.', ephemeral=True)
+            return False
 
     @discord.ui.button(label='Roll', style=discord.ButtonStyle.blurple)
     async def roll(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # if await self.interaction_check(user):
-        await interaction.response.send_message('Rolling...', ephemeral=True)
+        await interaction.response.defer(thinking=False)
+        await interaction.delete_original_response()
         self.value = True
         self.stop()
 
